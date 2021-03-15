3e23774abeb6f5240794326841ed0ce72c9838a7
KyleBloom <kyle.louis.bloom@gmail.com>
2021-03-15 14:49:13 -0500

    Adding Events to Elements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 
 import './App.css';
 
+const displayEmojiName = event => alert(event.target.id);
+
 function App() {
   const greeting = "greeting";
     return (
@@ -9,17 +11,23 @@ function App() {
       <p> I am writing JSX</p>
       <ul>
         <li>
-            <button>
+            <button
+              onClick={displayEmojiName}
+              >
               <span role="img" aria-label="grinning face" id="grinning face">😀</span>
             </button>
         </li>
         <li>
-          <button>
+          <button
+              onClick={displayEmojiName}
+              >
               <span role="img" aria-label="party popper" id="party popper">🎉</span>
           </button>
         </li>
         <li>
-          <button>
+            <button
+              onClick={displayEmojiName}
+              >
               <span role="img" aria-label="woman dancing" id="woman dancing">💃</span>
           </button>
         </li>
